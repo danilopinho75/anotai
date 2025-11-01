8405abecc5e4bf0920b7773b2873f3c32c986c8a
danilopinho75 <danilopinho11@gmail.com>
2025-11-01 19:45:01 -0300

    Criação do frontend do projeto

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Página Home</h1>
+  <router-link to="/produto">Ir para página de Produto</router-link>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/pages/Produto.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Página Produto</h1>
+  <router-link to="/">Ir para Página inicial</router-link>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+</style>
