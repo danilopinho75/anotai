4e9bf6d241273a4366ca99894a07783fd0a1ab7f
danilopinho75 <danilopinho11@gmail.com>
2025-11-01 20:01:36 -0300

    Configuração do tailwind

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,6 +1,12 @@
 <template>
-  <h1>Página Home</h1>
-  <router-link to="/produto">Ir para página de Produto</router-link>
+  <main class="flex flex-col w-full h-screen items-center justify-center gap-6 bg-red-500">
+    <h1 class="text-4xl font-semibold text-white">Página Home</h1>
+    <router-link to="/produto" class="
+    bg-white text-red-700 px-6 py-3 rounded-lg font-medium hover:bg-red-500
+    hover:text-white hover:border-1 hover:border-white transition duration-500">
+      Ir para página de Produto
+    </router-link>
+  </main>
 </template>
 
 <script setup>
