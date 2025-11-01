c6b25dead59955eaaa3708db6924ff14ddb30dcb
danilopinho75 <danilopinho11@gmail.com>
2025-11-01 19:33:42 -0300

    Atualizando rotas e dependencias

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const produtoRoutes = require('./routes/produto.routes');
 
 const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello guys!');
+  res.send('API Rodando!');
 })
+
+// Rotas
+app.use('/produtos', produtoRoutes);
 
 app.use((err, req, res, next) => {
   console.error(err);
