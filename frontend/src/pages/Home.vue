7a5a6cab1bdcac7edb2b9a2bbdb5d6e7754ea9fa
danilopinho75 <danilopinho11@gmail.com>
2025-11-03 21:11:07 -0300

    Boa prática do tailwind na Home

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -3,7 +3,7 @@
     <h1 class="text-4xl font-semibold text-white">Página Home</h1>
     <router-link to="/produto" class="
     bg-white text-red-700 px-6 py-3 rounded-lg font-medium hover:bg-red-500
-    hover:text-white hover:border-1 hover:border-white transition duration-500">
+    hover:text-white hover:border hover:border-white transition duration-500">
       Ir para página de Produto
     </router-link>
   </main>
