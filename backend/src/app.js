46eb22fd671e9a29c2a138e879dcbb3640885df9
danilopinho75 <danilopinho11@gmail.com>
2025-11-03 21:12:19 -0300

    Instalação do cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const produtoRoutes = require('./routes/produto.routes');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('API Rodando!');
